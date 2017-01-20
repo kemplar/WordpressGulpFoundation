5a916a17682a8755477d2247fc705f3233c5f235
kemplar <kdraving@gmail.com>
2017-01-20 10:13:35 -0500

    Jointswp-child

---
+++ jointswp-child/footer.php
@@ -0,0 +1,6 @@
+<script src="/wp-content/themes/jointswp-child/js/vendor/jquery.js"></script>
+<script src="/wp-content/themes/jointswp-child/js/vendor/foundation.js"></script>
+<script src="/wp-content/themes/jointswp-child/js/functions.js"></script>
+<script>$(document).foundation();</script>
+</body>
+</html>
